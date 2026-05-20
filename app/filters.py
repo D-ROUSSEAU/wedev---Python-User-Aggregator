eda66b0530c0d0f478be6c3f9c4c4de63aba2867
Damian <d.rousseau-pro@protonmail.com>
2026-05-20 10:59:44 +0200

    Rajout des commentaire explicatifs

--- app/filters.py
+++ app/filters.py
@@ -6,13 +6,17 @@ def filter_users_by_company_name(
     filtered_users = []
 
     for user in users:
+        # Récupère le champs "compagny" de l'utilisateur
         company = user.get("company")
 
+        # Si pas de champs, passe à l'utilisateur suivant
         if not company:
             continue
 
+        # Récupère le champs "name" du champs "compagny"
         company_name = company.get("name", "")
 
+        # Rajoute l'utilisateur au tableau filtered_users si "e" est dans le champs "name" de "compagny"
         if "e" in company_name.lower():
             filtered_users.append(user)
 
--- app/transformers.py
+++ app/transformers.py
@@ -7,8 +7,10 @@ def normalize_users(
     normalized_users: list[NormalizedUser] = []
 
     for user in users:
+        # Récupère le champs "address" de l'utilisateur
         address = user.get("address", {})
 
+        # Créer un NormalizedUser
         normalized_user: NormalizedUser = {
             "id": user.get("id", 0),
             "full_name": user.get("name", "Unknown"),
@@ -16,6 +18,7 @@ def normalize_users(
             "city": address.get("city", "Unknown")
         }
 
+        # Rajoute le NormalizedUser au tableau normalized_users
         normalized_users.append(normalized_user)
 
     return normalized_users
